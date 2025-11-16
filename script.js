7f8bcf2ab0600370062efbb638a6847a9fa070af
HorrySheet11 <marinnathan11@gmail.com>
2025-11-16 11:03:10 +0800

    made working fetch
    fetched weather with API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('loaded');
+
+const fetchWeather = async () => {
+    try {
+        const weather = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/philippines?unitGroup=us&key=AUGWL5T7N9X4NRPSDS535XT8E');
+        const data = await weather.json();
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+fetchWeather();
